5782d14dad8d08807529194b5d3c1e718622a0d3
gsolisve <gsolisve@gmail.com>
2024-10-26 11:53:49 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-# Reemplazar la URL del backend en tiempo de ejecución
+echo "Starting entrypoint script..."
+echo "BACKEND_URL is: $BACKEND_URL"
+
 if [ -n "$BACKEND_URL" ]; then
     echo "Configurando BACKEND_URL a: $BACKEND_URL"
-    find /usr/share/nginx/html -type f -name "main*.js" -exec sed -i "s|http://localhost:8080|$BACKEND_URL|g" {} +
+    find /usr/share/nginx/html -type f -name "main*.js" -exec sh -c '
+        echo "Procesando archivo: $1"
+        sed -i "s|http://localhost:8080|$BACKEND_URL|g" "$1"
+        echo "Contenido después del reemplazo:"
+        grep -r "$BACKEND_URL" "$1"
+    ' sh {} \;
 fi
 
-# Ejecutar el comando recibido (nginx en este caso)
+echo "Entrypoint script completed"
 exec "$@"
